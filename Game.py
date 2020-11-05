082a87453596eca89083c8b2524bcda6738e469b
Maciej Zyszkiewicz <kz_sky@outlook.com>
2020-11-05 21:45:23 +0100

    Game.py square movement input test

--- Game.py
+++ Game.py
@@ -2,19 +2,38 @@ import sys
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-box = pygame.Rect(10, 50, 100, 100)
+screenWidth = 1280
+screenHeight = 720
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+box = pygame.Rect(10, 50, 50, 50)
 
 while True:
     # Event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
+        elif event.type == pygame.KEYDOWN and event.type == pygame.K_q:
+            sys.exit(0)
 
     # Update
-    box.x += 1
-    box.y += 1
+
+    # box.x = (box.x + 1) % screenWidth
+    # box.y = (box.y + 1) % screenHeight
+
+    # box.x = pygame.mouse.get_pos()[0]
+    # box.y = pygame.mouse.get_pos()[1]
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        box.y = (box.y - 1) % screenHeight
+    if keys[pygame.K_a]:
+        box.x = (box.x - 1) % screenWidth
+    if keys[pygame.K_s]:
+        box.y = (box.y + 1) % screenHeight
+    if keys[pygame.K_d]:
+        box.x = (box.x + 1) % screenWidth
 
     # Drawing
+    screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (200, 0, 0), box)
     pygame.display.flip()
