055da09a0e2f3c8e04d2b6abf8c212f93f919af4
Maciej Zyszkiewicz <kz_sky@outlook.com>
2020-11-08 22:03:40 +0100

    Board.py add and remove methods

--- Board.py
+++ Board.py
@@ -5,5 +5,21 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.board = np.zeros((width, height))
+        self.points = np.zeros((width, height))
+        self.connections = set()
 
+    def add_connection(self, ax, ay, bx, by):
+        a = (ax, ay)
+        b = (bx, by)
+        ab = (a, b)
+        ba = (b, a)
+        if not (set.__contains__(ab) or set.__contains__(ba)):
+            set.add(ab)
+
+    def remove_connection(self, ax, ay, bx, by):
+        a = (ax, ay)
+        b = (bx, by)
+        ab = (a, b)
+        ba = (b, a)
+        set.remove(ab)
+        set.remove(ba)
