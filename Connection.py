de5237d2c0a6c633f4771837441767c142ae568d
Maciej Zyszkiewicz <kz_sky@outlook.com>
2020-12-04 05:09:34 +0100

    Connection class integration

--- Connection.py
+++ Connection.py
@@ -7,18 +7,18 @@ class Connection:
     Class representing a connection between two points on the field
     """
     def __init__(self, a, b, is_wall=False):
+        assert Connection.__validate_connection_length(a, b)
         self.a = a
         self.b = b
         self.is_wall = is_wall
 
-    def __validate_connection_length(self):
+    @staticmethod
+    def __validate_connection_length(a, b):
         """
         Checks if connection (A, B) is made between neighboring points
 
         :return: boolean
         """
-        a = self.a
-        b = self.b
         distance = math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
         if distance > 1.5 or distance == 0:
             return False
