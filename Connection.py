7e59ae6f92c7328b7e096498863a12b70c4cf3a7
Maciej Zyszkiewicz <kz_sky@outlook.com>
2020-12-04 04:43:25 +0100

    Validation

--- Connection.py
+++ Connection.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 class Connection:
@@ -8,3 +10,17 @@ class Connection:
         self.a = a
         self.b = b
         self.is_wall = is_wall
+
+    def __validate_connection_length(self):
+        """
+        Checks if connection (A, B) is made between neighboring points
+
+        :return: boolean
+        """
+        a = self.a
+        b = self.b
+        distance = math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+        if distance > 1.5 or distance == 0:
+            return False
+        else:
+            return True
