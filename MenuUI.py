18e6920978e190c4a3c1a368e6338601ff084876
aaron1699 <190224697@aston.ac.uk>
2020-12-05 17:33:56 +0000

    commented out line 48 in Game.py main menu now opens and game can be executed from there

--- MenuUI.py
+++ MenuUI.py
@@ -4,7 +4,8 @@ import pygame
 
 import Game
 
-
+bg = pygame.image.load("Art/Neon_theme1.png").convert_alpha()
+bg = pygame.transform.scale(bg, (1280, 720))
 screen = pygame.display.set_mode((Game.screenWidth, Game.screenHeight))
 font = pygame.font.SysFont(None, 50)
 
@@ -22,17 +23,17 @@ def main_menu():
         click = pygame.mouse.get_pressed()
         mouse = pygame.mouse.get_pos()
 
-        screen.fill((0, 0, 0))
-        draw_text('main menu test', font, (255, 255, 255), screen, 600, 20)
+        screen.blit(bg, (0, 0))
+        # draw_text('main menu test', font, (255, 255, 255), screen, 600, 20)
 
-        button_1 = pygame.Rect(50, 100, 200, 50)
-        button_2 = pygame.Rect(50, 200, 200, 50)
-        button_3 = pygame.Rect(50, 300, 200, 50)
+        button_1 = pygame.Rect(40, 100, 200, 50)
+        button_2 = pygame.Rect(1050, 100, 200, 50)
+        # button_3 = pygame.Rect(50, 300, 200, 50)
         pygame.draw.rect(screen, (255, 0, 0), button_1)
         pygame.draw.rect(screen, (0, 255, 0), button_2)
-        pygame.draw.rect(screen, (0, 0, 255), button_3)
+        # pygame.draw.rect(screen, (0, 0, 255), button_3)
         draw_text('start', font, (0, 0, 0), screen, 100, 100)
-        draw_text('exit', font, (0, 0, 0), screen, 100, 200)
+        draw_text('exit', font, (0, 0, 0), screen, 1110, 100)
         if button_1.collidepoint(mouse) and click[0] == 1:
             Game.main()
 
