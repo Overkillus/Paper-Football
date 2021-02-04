c155267c1433b987f4c9f9e6e8c320e85eb8acf8
aaron1699 <190224697@aston.ac.uk>
2021-02-04 00:55:23 +0000

    adeed buttons and background

--- View/SettingsUI.py
+++ View/SettingsUI.py
@@ -17,6 +17,18 @@ class SettingsUI:
     background = pygame.transform.scale(background, (Settings.screen_width, Settings.screen_height))
     setting_icon = pygame.image.load("Art/settings.png")
     exitIcon = pygame.image.load('Art/exit2.png')
+    default_screen = pygame.image.load('Art/default_b.png')
+    medium_screen = pygame.image.load('Art/medium_screen_b.png')
+    large_screen = pygame.image.load('Art/large_screen_b.png')
+    football_theme = pygame.image.load('Art/football_b.png')
+    neon_theme = pygame.image.load('Art/neon_b.png')
+    paper_theme = pygame.image.load('Art/paper_b.png')
+    small_board = pygame.image.load('Art/board_size_s.png')
+    medium_board = pygame.image.load('Art/board_size_m.png')
+    sound_off = pygame.image.load('Art/sound_off.png')
+    sound_reduce = pygame.image.load('Art/sound_reduce.png')
+    sound_increase = pygame.image.load('Art/sound_increase.png')
+
     font = pygame.font.SysFont('arialbold', 30)
 
     mixer.music.load('Sound/background.wav')
@@ -29,21 +41,22 @@ class SettingsUI:
         self.screen = controller.screen
         self.controller = controller
 
-        # Exit menu button
-        self.exit_rect = self.exitIcon.get_rect(topleft=(70, 80))
-
         # Buttons
         button_w = 70
         button_h = 40
-        self.button_1 = pygame.Rect(35, 150, button_w, button_h)
-        self.button_2 = pygame.Rect(135, 150, button_w, button_h)
-        self.button_3 = pygame.Rect(235, 150, button_w, button_h)
-        self.button_4 = pygame.Rect(90, 270, button_w, button_h)
-        self.button_5 = pygame.Rect(190, 270, button_w, button_h)
-        self.button_6 = pygame.Rect(90, 390, button_w, button_h)
-        self.button_7 = pygame.Rect(190, 390, button_w + 20, button_h)
-        self.button_8 = pygame.Rect(400, 150, button_w + 20, button_h)
-        self.button_9 = pygame.Rect(530, 150, button_w + 40, button_h)
+        self.exit_button = pygame.Rect(15, 25, self.setting_icon.get_width(), self.setting_icon.get_height())
+        self.default_screen_button = pygame.Rect(250, 95, self.default_screen.get_width(), self.default_screen.get_height())
+        self.medium_screen_button = pygame.Rect(350, 95, self.medium_screen.get_width(), self.medium_screen.get_height())
+        self.large_screen_button = pygame.Rect(450, 95, self.large_screen.get_width(), self.large_screen.get_height())
+        self.football_theme_button = pygame.Rect(225, 170, self.football_theme.get_width(), self.football_theme.get_height())
+        self.neon_theme_button = pygame.Rect(325, 170, self.neon_theme.get_width(), self.neon_theme.get_height())
+        self.paper_theme_button = pygame.Rect(425, 170, self.paper_theme.get_width(), self.paper_theme.get_height())
+        self.small_board_button = pygame.Rect(425, 170, self.small_board.get_width(), self.small_board.get_height())
+        self.medium_board_button = pygame.Rect(425, 170, self.medium_board.get_width(), self.medium_board.get_height())
+
+
+
+
 
     def main(self):
         self.event_handler()
@@ -64,14 +77,14 @@ class SettingsUI:
                 # Mouse click
                 mouse_pos = pygame.mouse.get_pos()
                 # Increase volume
-                if self.button_1.collidepoint(mouse_pos):
-                    mixer.music.set_volume(0.25)
-                elif self.button_2.collidepoint(mouse_pos):
-                    mixer.music.set_volume(0.5)
-                elif self.button_3.collidepoint(mouse_pos):
-                    mixer.music.set_volume(1.0)
+                #if self.button_1.collidepoint(mouse_pos):
+                 #   mixer.music.set_volume(0.25)
+                #elif self.button_2.collidepoint(mouse_pos):
+                 #   mixer.music.set_volume(0.5)
+                #elif self.button_3.collidepoint(mouse_pos):
+                 #   mixer.music.set_volume(1.0)
                 # Back button
-                elif self.exit_rect.collidepoint(mouse_pos):
+                if self.exit_button.collidepoint(mouse_pos):
                     self.controller.change_view(self.controller.menuUI)
 
     def update(self):
@@ -85,41 +98,65 @@ class SettingsUI:
         self.screen.blit(self.background, (0, 0))
 
         # Settings title
-        draw_text('Game Settings', self.font, Colours.WHITE, self.screen, 270, 30)
+        draw_text('Game Settings', self.font, Colours.WHITE, self.screen, 300, 30)
 
         # Exit Icon
         self.screen.blit(self.exitIcon, (15, 25))
 
+        # Resolution
+        draw_text('Resolution:', self.font, Colours.WHITE, self.screen, 125, 100)
+        self.screen.blit(self.default_screen, (250, 95))
+        self.screen.blit(self.medium_screen, (350, 95))
+        self.screen.blit(self.large_screen, (450, 95))
+
+        # Themes
+        draw_text('Themes:', self.font, Colours.WHITE, self.screen, 125, 175)
+        self.screen.blit(self.football_theme, (225, 170))
+        self.screen.blit(self.neon_theme, (325, 170))
+        self.screen.blit(self.paper_theme, (425, 170))
+
+        # Board size
+        draw_text('Board Size:', self.font, Colours.WHITE, self.screen, 125, 250)
+        self.screen.blit(self.small_board, (250, 245))
+        self.screen.blit(self.medium_board, (320, 245))
+        # Colours
+        draw_text('Select your colour:', self.font, Colours.WHITE, self.screen, 125, 325)
+        # Sound
+        draw_text('Sound:', self.font, Colours.WHITE, self.screen, 125, 400)
+        self.screen.blit(self.sound_off, (220, 385))
+        self.screen.blit(self.sound_reduce, (270, 385))
+        self.screen.blit(self.sound_increase, (320, 385))
+
         # Volume Buttons
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_1)
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_2)
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_3)
-        draw_text('25%', self.font, Colours.WHITE, self.screen, self.button_1.x + 18, self.button_1.y + 10)
-        draw_text('50%', self.font, Colours.WHITE, self.screen, self.button_2.x + 18, self.button_2.y + 10)
-        draw_text('100%', self.font, Colours.WHITE, self.screen, self.button_3.x + 8, self.button_3.y + 10)
+       # pygame.draw.rect(self.screen, Colours.GREY, self.button_1)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_2)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_3)
+        #draw_text('25%', self.font, Colours.WHITE, self.screen, self.button_1.x + 18, self.button_1.y + 10)
+        #draw_text('50%', self.font, Colours.WHITE, self.screen, self.button_2.x + 18, self.button_2.y + 10)
+        #draw_text('100%', self.font, Colours.WHITE, self.screen, self.button_3.x + 8, self.button_3.y + 10)
 
         # Theme Buttons
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_4)
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_5)
-        draw_text('Neon', self.font, Colours.WHITE, self.screen, self.button_4.x + 8, self.button_4.y + 10)
-        draw_text('Paper', self.font, Colours.WHITE, self.screen, self.button_5.x + 8, self.button_5.y + 10)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_4)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_5)
+        #draw_text('Neon', self.font, Colours.WHITE, self.screen, self.button_4.x + 8, self.button_4.y + 10)
+        #draw_text('Paper', self.font, Colours.WHITE, self.screen, self.button_5.x + 8, self.button_5.y + 10)
 
         # Board Size
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_6)
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_7)
-        draw_text('13 x 9', self.font, Colours.WHITE, self.screen, self. button_6.x + 8, self.button_6.y + 10)
-        draw_text('19 x 15', self.font, Colours.WHITE, self.screen, self.button_7.x + 8, self.button_7.y + 10)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_6)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_7)
+        #draw_text('13 x 9', self.font, Colours.WHITE, self.screen, self. button_6.x + 8, self.button_6.y + 10)
+        #draw_text('19 x 15', self.font, Colours.WHITE, self.screen, self.button_7.x + 8, self.button_7.y + 10)
 
         # Screen Mode
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_8)
-        pygame.draw.rect(self.screen, Colours.GREY, self.button_9)
-        draw_text('Full', self.font, Colours.WHITE, self.screen, self.button_8.x + 8, self.button_8.y + 10)
-        draw_text('Windowed', self.font, Colours.WHITE, self.screen, self.button_9.x + 8, self.button_9.y + 10)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_8)
+        #pygame.draw.rect(self.screen, Colours.GREY, self.button_9)
+        #draw_text('Full', self.font, Colours.WHITE, self.screen, self.button_8.x + 8, self.button_8.y + 10)
+        #draw_text('Windowed', self.font, Colours.WHITE, self.screen, self.button_9.x + 8, self.button_9.y + 10)
         # Headers
-        draw_text('Volume', self.font, Colours.WHITE, self.screen, 135, 110)
-        draw_text('Theme', self.font, Colours.WHITE, self.screen, 135, 230)
-        draw_text('Board Size', self.font, Colours.WHITE, self.screen, 115, 350)
-        draw_text('Screen Mode', self.font, Colours.WHITE, self.screen, 450, 110)
+        #draw_text('Volume', self.font, Colours.WHITE, self.screen, 135, 110)
+        #draw_text('Theme', self.font, Colours.WHITE, self.screen, 135, 230)
+        #draw_text('Board Size', self.font, Colours.WHITE, self.screen, 115, 350)
+        #draw_text('Screen Mode', self.font, Colours.WHITE, self.screen, 450, 110)
 
         # Show new frame
         pygame.display.flip()
