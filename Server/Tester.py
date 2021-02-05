22c2d7053c5397ca155243aa80c8f108e856c808
EVILSHOOTER <72513936+EVILSHOOTER@users.noreply.github.com>
2021-02-05 17:14:56 +0000

    UNIT testing script
    not really packed with tests. IF YOU CAN think of any more, please add them in the tests. should be kinda easy.
    
    i couldn't really come up with much considering some functions encapsulate the majority of functionality within one function (scripts are kinda daisy-chained together through sending meaningful messages).

--- Server/Tester.py
+++ Server/Tester.py
@@ -9,15 +9,26 @@ import socket
 import unittest
 from test import support
 
-class TestCase1(unittest.TestCase):
-    def setUp(self):
-        self.serverManager = ServerManager(socket.gethostname(), 2000)
-        #self.gameServer = GameServer(socket.gethostname(), 2001)
-        self.client = Client(socket.gethostname(), 2000)
+#serverManager = ServerManager(socket.gethostname(), 2000)
+# gameServer = GameServer(socket.gethostname(), 2001)
+#client = Client(socket.gethostname(), 2000)
 
-    def tearDown(self):
+class TestCase1(unittest.TestCase):
+    # I think this script only works when run in isolation with just the Server folder.
+    # anything else and the socket will be used by other scripts.
+
+    def setUp(self): # run at start of every test
+        self.serverManager = ServerManager(socket.gethostname(), 2000)
+        # gameServer = GameServer(socket.gethostname(), 2001)
+        self.client = Client(socket.gethostname(), 2000)
         pass
-        # server and client disconnect?
+
+    def tearDown(self): # run at end of every test
+        #self.client.disconnect()
+        self.serverManager.SERVER_ON = False # closes server. THIS IS UGLY, MAKE A FUNC FOR THIS SOON.
+        time.sleep(0.5)
+        pass
+        # server and client disconnect? closing both connections?
 
     # since these tests are asynchronous, waiting is required at times.
 
@@ -27,6 +38,7 @@ class TestCase1(unittest.TestCase):
     def test2(self): # client create server. this performs the join too.
         self.client.create_server()
 
+"""
     def test3(self): # client leave server
         self.client.disconnect()
 
@@ -35,6 +47,7 @@ class TestCase1(unittest.TestCase):
 
     def test5(self): # client sends pointless message
         self.client.send_to_server("jv(w$jowjty($")
+"""
 
 if __name__ == '__main__':
     unittest.main()
