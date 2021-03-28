ca2a7b524b3b5e5049825e3d4f3bfebe5a4dc6a4
Maciej Zyszkiewicz <kz_sky@outlook.com>
2021-03-28 18:03:08 +0200

    credits 2 page

--- View/CreditsUI.py
+++ View/CreditsUI.py
@@ -16,17 +16,21 @@ class CreditsUI:
     title = pygame.font.SysFont('comicsansms', 50)
     font = pygame.font.SysFont('comicsansms', 18)
     exit_icon = pygame.image.load("Art/Neon/exit2.png")
-    credits = pygame.image.load("Art/Neon/credits_screen.png")
+    next_icon = pygame.transform.flip(exit_icon, True, False)
+    credits1 = pygame.image.load("Art/Neon/credits1_screen.png")
+    credits2 = pygame.image.load("Art/Neon/credits2_screen.png")
 
     def __init__(self, controller):
         # State
         self.is_running = False
+        self.credits_page = 1
 
         # Context
         self.screen = controller.screen
         self.controller = controller
         self.exit_icon_rect = self.exit_icon.get_rect()
-        self.credits_rect = self.credits.get_rect()
+        self.next_icon_rect = self.next_icon.get_rect()
+        self.credits_rect = self.credits1.get_rect()
 
     def main(self):
         self.event_handler()
@@ -45,7 +49,13 @@ class CreditsUI:
                 self.controller.change_view(self.controller.menuUI)
             elif event.type == pygame.MOUSEBUTTONUP:
                 if self.exit_icon_rect.collidepoint(pygame.mouse.get_pos()):
-                    self.controller.change_view(self.controller.menuUI)
+                    if self.credits_page > 1:
+                        self.credits_page -= 1
+                    else:
+                        self.controller.change_view(self.controller.menuUI)
+                if self.next_icon_rect.collidepoint(pygame.mouse.get_pos()):
+                    if self.credits_page < 2:
+                        self.credits_page += 1
 
     def update(self):
         # Layout helper variables
@@ -53,6 +63,7 @@ class CreditsUI:
         sh = self.screen.get_height()
 
         self.exit_icon_rect.topleft = (20, 20)
+        self.next_icon_rect.topright = (sw-20, 20)
         self.credits_rect.center = (sw/2, sh/2)
 
     def render(self):
@@ -60,10 +71,14 @@ class CreditsUI:
         self.screen.fill((3, 15, 56))
 
         # Credits Screen
-        self.screen.blit(self.credits, self.credits_rect)
+        if self.credits_page == 1:
+            self.screen.blit(self.credits1, self.credits_rect)
+        elif self.credits_page ==2:
+            self.screen.blit(self.credits2, self.credits_rect)
 
         # Button
         self.screen.blit(self.exit_icon, self.exit_icon_rect)
+        self.screen.blit(self.next_icon, self.next_icon_rect)
 
         # Show new frame
         pygame.display.flip()
